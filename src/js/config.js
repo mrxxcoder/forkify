406d1d2a884d0b30fa168bdf033c6471a920b6c4
mrxxcoder <romirain50x@gmail.com>
2023-04-05 02:45:48 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '07a5374f-e3b8-43ab-a8f3-d5159f387c9a';
+export const MODAL_CLOSE_SEC = 2.5;
